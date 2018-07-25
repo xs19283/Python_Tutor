59b1a88156e2e5836246d21970c899e6beb6ea68
xs19283 <xs19283@gmail.com>
2018-07-25 18:42:14 +0800

    NEW Tutor

--- Python/Ch02.py
+++ Python/Ch02.py
@@ -1,3 +1,5 @@
+import sys
+
 class add():
 
     def __init__(self):
@@ -9,17 +11,42 @@ class add():
 
 
 a = add()
-
 print(a.add(1, 2))
+
 """
 try:
     while 1:
         print(0)
 except KeyboardInterrupt:
     print(123)
-"""
-f = open("555.txt",'a+')
+
+f = open("555.txt", 'a+')
 print(type(f))
-fvalue =f.readline()
+fvalue = f.read()
 print(fvalue)
 f.write(" 222")
+
+
+class animals():
+    def __init__(self, name):
+        self.name = name
+
+a=animals("蔡明智")
+print(a.name)
+
+class Team():
+    def __init__(self, TeamName):
+        self.TeamName=TeamName
+
+s1 = "abcdefg"
+
+print(s1[:-2])
+"""
+
+sdf = [2,5,8,43,7,6,4]
+sdf.sort()
+print(sdf)
+
+a = lambda x : x+9
+print(a(5))
+
